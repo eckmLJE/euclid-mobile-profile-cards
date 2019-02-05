d3743ed9e8868220e97b206af419ec7b47e0a3ff
Lucas Eckman <lucas.eckman@gmail.com>
2019-02-05 15:32:09 -0500

    toggle expand-collapse in xs header

--- index.js
+++ index.js
@@ -9,9 +9,27 @@ function calculateLayout(e) {
   console.log(e);
 }
 
-$("#expand-all-button").click(function() {
+$("#expand-all-button").click(expandAll);
+$("#collapse-all-button").click(collapseAll);
+$("#expand-all-button-sm").click(expandAllSm);
+$("#collapse-all-button-sm").click(collapseAllSm);
+
+function expandAll() {
   $(".collapse").collapse("show");
-});
-$("#collapse-all-button").click(function() {
+}
+
+function collapseAll() {
   $(".collapse").collapse("hide");
-});
+}
+
+function expandAllSm() {
+  expandAll();
+  $("#expand-button-div-sm").toggleClass("d-none");
+  $("#collapse-button-div-sm").toggleClass("d-none");
+}
+
+function collapseAllSm() {
+  collapseAll();
+  $("#expand-button-div-sm").toggleClass("d-none");
+  $("#collapse-button-div-sm").toggleClass("d-none");
+}
